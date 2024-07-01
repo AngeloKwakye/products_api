f4c81fb6c7da0c0ba7c66dca8dede80f1b2e18ec
AngeloKwakye <dangelokwakye@gmail.com>
2024-07-01 12:27:01 +0000

    added routes, created controller with create,read,update and delete functions. also added search and pagination to response data

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -20,6 +20,7 @@ class ProductController extends Controller
         // echo "jsbjdjdd"; die;
     }
 
+    //create new product
     public function create(Request $request){
         //for validating required fields
         $validateProduct = Validator::make($request->all(),[
@@ -27,17 +28,106 @@ class ProductController extends Controller
             'quantity'=>'required|numeric',
             'price'=> 'required|decimal:0,2'
         ]);
+        //error message if validation fails
         if($validateProduct->fails()){
             return response()->json([
                 'status' => false,
                 'message' => 'Validation error',
                 'data' => $validateProduct->errors()
-            ]);
+            ],422);
         }
-       /*  return response()->json([
+        //if validation is successful, create an array of the incoming data;
+        $inputData = array(
+            'product_name' => $request->product_name,
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+            //the {isset} is responsible for checking if a variable is set is not null;
+            //the {isset} function returns false if  the variable being checked is null;
+            'description' => isset($request->description) ? $request->description: '',
+        );
+        //create new product using Products model
+        $product = Product::create($inputData);
+        //return success response
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Product created successfully!',
+            'data'=> $product
+        ],200);
+    }
+
+    //update product
+    public function update(Request $request){
+        //for validating required fields
+        $validateProduct = Validator::make($request->all(),[
+            'product_id'=> 'required|exists:products,id',
+            'product_name'=> 'required',
+            'quantity'=>'required|numeric',
+            'price'=> 'required|decimal:0,2'
+        ]);
+        //error message if validation fails
+        if($validateProduct->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'data' => $validateProduct->errors()
+            ],422);
+        }
+
+        //find product with the id using Products model
+        $product = Product::find($request->product_id);
+        //update product fields
+        $product->product_name = $request->product_name;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->description = isset($request->description) ? $request->description: '';
+        //save product
+        $product->save();
+        //return success response
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Product updated successfully!',
+            'data'=> $product
+        ],200);
+    }
+
+    //delete product
+    public function delete(Request $request){
+        //for validating required fields
+        $validateProduct = Validator::make($request->all(),[
+            'product_id'=> 'required|exists:products,id'
+        ]);
+        //error message if validation fails
+        if($validateProduct->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'data' => $validateProduct->errors()
+            ],422);
+        }
+
+        //find product with the id using Products model and delete
+        $product = Product::find($request->product_id)->delete();
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Product deleted successfully!',
+        ],200);
+    }
+
+     //search listings
+     public function search(Request $request){
+        $products = Product::where('product_name','!=','');
+        if(isset($request->product_name) && $request->product_name != ''){
+            $products = $products->where('product_name', $request->product_name);
+        };
+        if(isset($request->quantity) && $request->quantity != ''){
+            $products = $products->where('quantity', $request->quantity);
+        };
+        $products = $products->paginate(2);
+        return response()->json([
             'status'=> true,
             'message'=> 'Products listed successfully',
-            'data'=> $product
-        ],200); */
+            'data'=> $products
+        ],200);
+        // echo "jsbjdjdd"; die;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Api\ProductController;
 
 Route::get('products',[ProductController::class, 'index']);
 Route::post('create-product',[ProductController::class, 'create']);
+Route::put('update-product',[ProductController::class, 'update']);
+Route::delete('delete-product',[ProductController::class, 'delete']);
+Route::post('search',[ProductController::class, 'search']);
 /* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum'); */
